9f2be3b1c08bc35088e9e1b5d71984d5023b882d
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-11 11:14:44 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const twit = require('twit');
+const T = new twit({
+    consumer_key: process.env.APPLICATION_CONSUMER_KEY_HERE,
+    consumer_secret: process.env.APPLICATION_CONSUMER_SECRET_HERE,
+    access_token: process.env.ACCESS_TOKEN_HERE,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET_HERE
+})

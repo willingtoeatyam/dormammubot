d2e2c9c659229f44c5e4f79d458555130cb164fa
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-26 19:16:41 +0100

    selecting from database

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ app.post('', (req, res) => {})
 function tweetIt(){
     db.select('item').from('counter').then(data => {
         console.log('CDQ yeah yeah', data);
-        console.log(data[0]);
-        console.log(data[0][0]);
         console.log(data[0].item[0]);
         console.log('nkc');
+        let p = data[0].item[0];
    });
   
-    const text = {status: `Dormammu, I've come to bargain. This is the ${ordinal.toWordsOrdinal(n)} time.`}
+   console.log(p);
+   const text = {status: `Dormammu, I've come to bargain. This is the ${ordinal.toWordsOrdinal(n)} time.`}
 
     T.post('statuses/update', text , function(error, tweet, response) {
         if(error){

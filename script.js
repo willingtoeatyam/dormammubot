d2ffb97d81eff63ed81407a8aa98098511479f64
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-26 20:41:42 +0100

    selecting from database

--- script.js
+++ script.js
@@ -17,37 +17,36 @@ const db = knex({
     }
 });
 
-let q = 5;
+let q = 1;
 let n = 1;
 callNo();
 tweetIt();
-setInterval(tweetIt, 1800000);
-
-app.get('', (req, res) => {})
-app.post('', (req, res) => {})
+setInterval(tweetIt, 18000); //1800000
 
 function callNo(){
     db.select('item').from('counter').then(data => {
         if(true){
             console.log(data[0].item[0]);
             q = data[0].item[0];
+            console.log('callNo', q);
         }
    });
 }
 
 function tweetIt(){
-   const text = {status: `Dormammu, I've come to bargain. This is the ${ordinal.toWordsOrdinal(n)} time.`}
+   const text = {status: `Dormammu, I've come to bargain. This is the ${ordinal.toWordsOrdinal(q)} time.`}
     T.post('statuses/update', text , function(error, tweet, response) {
         if(error){
             console.log(error)
         } else {
             console.log(text);
-            console.log('please', q);
+            console.log('tweetIt', q);
         }
     })
     increaseCount();
 }
 
 function increaseCount(){
-    n++;
+    db('counter').update('item', 'item + 1');
+    console.log('increaseCount', q);
 }

c5658424c43a2da232efe69c5288f01aca162c53
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-27 15:23:05 +0100

    selecting from database

--- script.js
+++ script.js
@@ -25,20 +25,18 @@ setInterval(botWork, 60000); //1800000
 function botWork(){
     db('counter').where({id: '1'}).select('item').then(data => {
         if(data[0].item == q){
-            let p = data;
-            console.log('knex fixed', p[0].item);
+            q = data[0].item;
+            console.log('knex fixed', q);
             tweetIt();
         }
     });
 
-    db.select('item').from('counter').then(data => {
-        if(true){
-            q = data[0].item;
-            console.log('received', q);
-        }
-   });
-
-   console.log('after received from db', q);
+//     db.select('item').from('counter').then(data => {
+//         if(true){
+//             q = data[0].item;
+//             console.log('received', q);
+//         }
+//    });
 
    function tweetIt(){
         const text = {status: `Dormammu, I've come to bargain. This is the ${ordinal.toWordsOrdinal(q)} time.`}
@@ -49,19 +47,11 @@ function botWork(){
                 console.log(text);
                 console.log('value tweeted', q);
                 db('counter').where('id', '=', 1).increment('item', 1).then(data => {
-                    let c = data;
-                    console.log('value after increase', c);
+                    console.log('succesfully increased by 1');
                 })   
             }
         })
     }
-}
 
-function increaseCount(){
-    db.select('item').from('counter').then(data => {
-        if(data[0].item == q){
-            let l = data[0].item;
-            console.log('value before increase', l);
-        }
-    });
+
 }
